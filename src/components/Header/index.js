1daa76163544f4eb1e59c18725ff03391a93352b
adcar <13227184+adcar@users.noreply.github.com>
2018-04-02 12:46:50 -0400

    Remove unneeded icons

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -14,13 +14,6 @@ import Typography from 'material-ui/Typography'
 import List from 'material-ui/List'
 import { ListItem, ListItemIcon, ListItemText } from 'material-ui/List'
 
-import InboxIcon from 'material-ui-icons/MoveToInbox'
-import DraftsIcon from 'material-ui-icons/Drafts'
-import StarIcon from 'material-ui-icons/Star'
-import SendIcon from 'material-ui-icons/Send'
-import MailIcon from 'material-ui-icons/Mail'
-import DeleteIcon from 'material-ui-icons/Delete'
-import ReportIcon from 'material-ui-icons/Report'
 import MenuIcon from 'material-ui-icons/Menu'
 
 const styles = theme => ({
