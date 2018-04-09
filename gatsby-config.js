3fedc26685fd24dcc3cf99a9e3e289108b31f504
MD5HashBrowns <md5hashbrownz@gmail.com>
2018-04-09 19:01:22 -0400

    Added service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,21 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Gatsby Default Starter'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
-};
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'GatsbyJS',
+        short_name: 'GatsbyJS',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#a2466c',
+        display: 'minimal-ui',
+        icon: 'src/img/uprightBuilders.png' // This path is relative to the root of the site.
+      }
+    },
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-offline'
+  ]
+}
